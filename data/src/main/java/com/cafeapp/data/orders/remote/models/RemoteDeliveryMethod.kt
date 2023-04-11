1001eb3ce7dcaf523b82399499461a5d683166a7
zzzondy <artemr19032006@gmail.com>
2023-04-11 15:29:00 +0300

    Minor updates

--- data/src/main/java/com/cafeapp/data/orders/remote/models/RemoteDeliveryMethod.kt
+++ data/src/main/java/com/cafeapp/data/orders/remote/models/RemoteDeliveryMethod.kt
@@ -1,12 +1,19 @@
 package com.cafeapp.data.orders.remote.models
 
+import com.google.firebase.firestore.PropertyName
+
 data class RemoteDeliveryMethod(
+
+    @PropertyName("id")
     val id: Long = 0,
 
+    @PropertyName("name")
     val name: String = "",
 
+    @PropertyName("fullAddress")
     val fullAddress: String = "",
 
+    @PropertyName("isDelivery")
     @field:JvmField
     val isDelivery: Boolean = true
 )
--- data/src/main/java/com/cafeapp/data/orders/remote/models/RemotePaymentMethod.kt
+++ data/src/main/java/com/cafeapp/data/orders/remote/models/RemotePaymentMethod.kt
@@ -1,6 +1,12 @@
 package com.cafeapp.data.orders.remote.models
 
+import com.google.firebase.firestore.PropertyName
+
 data class RemotePaymentMethod(
+
+    @PropertyName("id")
     val id: Long = 0,
+
+    @PropertyName("name")
     val name: String = ""
 )
